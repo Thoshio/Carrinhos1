579490e307a45b83147cffe0501edeb503a277ff
Thoshio Onuki Neto <thoshio@terra.com.br>
2026-09-22 01:27:38 -0300

    fix: redefining 90deg time

--- lib/motors/motors.h
+++ lib/motors/motors.h
@@ -17,7 +17,7 @@
 // Define o valor do registrador MOD do TPM para configurar o período do PWM
 #define TPM_MODULE_MOTORS 10000
 #define POT_STRAIGHT 60
-#define TEMPO_CURVA_90_MS 1200
+#define TEMPO_CURVA_90_MS 3500
 
 // Configuracao
 void motors_init(void);
